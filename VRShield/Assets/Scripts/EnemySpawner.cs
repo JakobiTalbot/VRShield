26920138485a7206254a651386084d5a70ba2320
JoshMosk <josh.piotr@gmail.com>
2019-06-06 16:41:59 +1000

    started on the radar, commented enemySpawner script

--- VRShield/Assets/Scripts/EnemySpawner.cs
+++ VRShield/Assets/Scripts/EnemySpawner.cs
@@ -4,39 +4,36 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public float m_spawnTimer = 3.0f;
+    public float m_spawnTimer = 3.0f;       //the time between enemy spawns
 
-    public Vector2 m_spawnMinMaxHeight;
-    public float m_spawnRadius;
-    public GameObject m_enemyPrefab;
+    public Vector2 m_spawnMinMaxHeight;     //the min and max height of the spawns
+    public float m_spawnRadius;     //the radius at which the enemies spawn
+    public GameObject m_enemyPrefab;        //reference to the enemy prefab
 
-    private float m_timer;
+    private float m_timer;      //actual timer for enemy spawn
 
     private void Start()
     {
-        m_timer = m_spawnTimer;
+        m_timer = m_spawnTimer;     //set spawn time
     }
 
     private void Update()
     {
-        m_timer -= Time.deltaTime;
+        m_timer -= Time.deltaTime;      //countdown time
 
-        if (m_timer <= 0)
+        if (m_timer <= 0)       //if time has run out
         {
-            SpawnEnemy();
-            m_timer += m_spawnTimer;
+            SpawnEnemy();       //spawn enemy
+            m_timer += m_spawnTimer;        //reset time
         }
     }
 
-    public void SpawnEnemy()
+    public void SpawnEnemy()        //spawn the enemy
     {
-        Vector2 tempLoc = Random.insideUnitCircle.normalized * m_spawnRadius;
-        Vector3 spawnLoc = new Vector3(tempLoc.x, 0, tempLoc.y);
-        spawnLoc.y = Random.Range(m_spawnMinMaxHeight.x, m_spawnMinMaxHeight.y);
+        Vector2 tempLoc = Random.insideUnitCircle.normalized * m_spawnRadius;       //find the x and z coord for where to put enemy
+        Vector3 spawnLoc = new Vector3(tempLoc.x, Random.Range(m_spawnMinMaxHeight.x, m_spawnMinMaxHeight.y), tempLoc.y);        //puts x y z together
 
-        //Vector3 v3SpawnLocation = Random.insideUnitSphere.normalized * m_spawnRadius;
-        //v3SpawnLocation.y = Mathf.Abs(v3SpawnLocation.y);
-        GameObject temp = Instantiate(m_enemyPrefab);
-        temp.transform.position = spawnLoc;
+        GameObject temp = Instantiate(m_enemyPrefab);       //creates enemy
+        temp.transform.position = spawnLoc;     //puts enemy into position
     }
 }
